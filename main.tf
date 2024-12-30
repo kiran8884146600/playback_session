3c26be21a59b77455dc1304060d13bf7614afac2
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-30 10:22:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,14 @@
 provider "aws" {
-  region = var.aws_region
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
  
 resource "aws_instance" "example" {
   ami           = var.ami_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
  
   tags = {
-    Name = "example-instance"
+    Name = "my-instnc"
   }
-}
- 
-output "instance_id" {
-value = aws_instance.example.id
-  sensitive = true
 }
