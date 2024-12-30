2834d437c031af96cafe9ab473a75ebbbc48be34
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-30 11:00:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,26 @@
-variable "aws_region" {
-  description = "AWS Region"
+variable "region" {
+  description = "AWS region"
   type        = string
   default     = "us-east-1"
-}
- 
-variable "ami_id" {
-  description = "AMI ID"
-  type        = string
-  sensitive   = true
 }
  
 variable "access_key" {
   description = "AWS Access Key"
   type        = string
-  sensitive   = true
 }
  
 variable "secret_key" {
   description = "AWS Secret Key"
   type        = string
-  sensitive   = true
 }
  
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+ 
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
