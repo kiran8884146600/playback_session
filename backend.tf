deb5ca6670a87d17e5b97e2d8905275e64014f29
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-30 12:38:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "my-bucket-terra"
-    key            = "my-bucket-terra"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket = "mybucket"
+    key    = "my-bucket-terra"
+    region = "us-east-1"
   }
 }
