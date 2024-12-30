5d3a866c29d5b758eb9a0ebd903c6c5392097041
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-30 11:15:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "my-bucket-terra"
-    key            = "terraform/state"
+    bucket         = "my-bucket-terra-state"
+    key            = "my-bucket-terra "
     region         = var.region
     access_key     = var.access_key
     secret_key     = var.secret_key
