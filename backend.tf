82624b9a33209e4d052938d74ca90650127d2bb7
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-31 14:42:35 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,11 @@
+# backend.tf - Root-level backend configuration
+
+terraform {
+  backend "s3" {
+    bucket         = "kiran271222"
+    key            = "rules/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "kiran-db"
+  }
+}
